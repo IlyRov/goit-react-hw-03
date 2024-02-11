0dcf6605e98980dd7e54196559a120933cad0739
IlyRov <127045335+IlyRov@users.noreply.github.com>
2024-02-11 23:26:08 +0000

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,7 +5,7 @@ import SearchBox from './SearchBox';
 
 const App = () => {
   const initialContacts = [
-    { id: 'id-1', name: 'Rosie Simpson', phone: '459-12-56' },
+    { id: 'id-1', name: 'Rosie Simpson', phone: '469-12-56' },
     { id: 'id-2', name: 'Hermione Kline', phone: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', phone: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', phone: '227-91-26' },
